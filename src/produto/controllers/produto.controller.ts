4f76c01cf7c022fb780296f5a0bfdafdcd84d75f
vmonea <vitormonea2000@gmail.com>
2024-05-03 09:40:43 -0300

    Criado o CRUD do produto

---
+++ src/produto/controllers/produto.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Body, Put, Delete } from "@nestjs/common";
+import { Produto } from "../entities/produto.entity";
+import { ProdutoService } from "../services/produto.service";
+
+
+
+@Controller("/postagens")
+export class ProdutoController{
+
+    constructor(private readonly produtoService: ProdutoService){}
+
+    @Get()
+    @HttpCode(HttpStatus.OK) // Status 200 do HTTP
+    findAll(): Promise<Produto[]>{
+        return this.produtoService.findAll();
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK) // Status 200 do HTTP
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Produto>{
+        return this.produtoService.findById(id);
+    }
+
+    @Get('/nome/:nome')
+    @HttpCode(HttpStatus.OK) // Status 200 do HTTP
+    findBy(@Param('nome') nome: string): Promise<Produto[]>{
+        return this.produtoService.findByNome(nome);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() produto: Produto): Promise<Produto>{
+        return this.produtoService.create(produto);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() produto: Produto): Promise<Produto>{
+        return this.produtoService.update(produto);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT) // Status 204 do HTTP
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.produtoService.delete(id);
+    }
+
+}
---
+++ src/produto/produto.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CategoriaModule } from "../categoria/categoria.module";
+import { CategoriaService } from "../categoria/services/categoria.service";
+import { Produto } from "./entities/produto.entity";
+import { ProdutoService } from "./services/produto.service";
+import { ProdutoController } from "./controllers/produto.controller";
+
+
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Produto]), CategoriaModule],
+    providers: [ProdutoService, CategoriaService],
+    controllers: [ProdutoController],
+    exports: [TypeOrmModule]
+})
+
+export class ProdutoModule{ }
