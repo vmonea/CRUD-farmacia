65cfef48c0c7c49eb5f6f8b2f1ed99bda35b5bf9
vmonea <vitormonea2000@gmail.com>
2024-05-03 09:38:38 -0300

    Criado o CRUD da categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
+import { ProdutoModule } from './produto/produto.module';
+import { Produto } from './produto/entities/produto.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_farmacia',
+      entities:[Categoria, Produto],
+      synchronize: true,
+      logging: true,
+    }),
+CategoriaModule,
+ProdutoModule
+
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
